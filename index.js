30d9184133336e5ff8719c728e92d6abaf21091e
mihirchauhan29 <47627733+mihirchauhan29@users.noreply.github.com>
2024-03-14 19:35:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app=express()
 const dotenv= require('dotenv').config()
-const PORT=process.env|| 3000
+const PORT=process.env.PORT|| 3000
 const connect= require('./config/dbConnect')
 
 
